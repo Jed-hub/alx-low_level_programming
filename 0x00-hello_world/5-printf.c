eb75be8bd46e7abbea325a34d5f444e1b0f30828
Jed <jedbahena@gmail.com>
2022-03-10 05:14:27 -0800

    Ok

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,6 +3,6 @@
 /*This program prints a message */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf ("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
