3f2ceddaca630b94901834e06f8f28c2f2c13bf7
Jed <jedbahena@gmail.com>
2022-03-10 06:10:33 -0800

    Ok

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - Prints with proper grammar, but the outcome is a piece of art,
- *
- * Return:Always 0 (Success)
+ * main - Prints a sentence.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
