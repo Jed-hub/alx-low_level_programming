656b1cb61109de2c59c228d76810fb3539e98a6d
Jed <jedbahena@gmail.com>
2022-03-10 15:50:54 -0800

    Ok

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a text
+ *
+ * Return: Always 1
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" -Dora Kopar, 2015-10-19\n");
+	return (1);
+}
