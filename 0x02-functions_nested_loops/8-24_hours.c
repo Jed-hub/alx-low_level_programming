b20ae789688522b394c31d1933a75b944e360741
Jed <jedbahena@gmail.com>
2022-03-15 16:45:50 -0700

    Ok

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,30 +9,18 @@
  */
 void jack_bauer(void)
 {
-	int n, m, l, k;
+	int n, m;
 
-	for (n = 48; n <= 50; n++)
+	for (n = 0; n <= 23; n++)
 	{
-		for (m = 48; m <= 51; m++)
+		for (m = 0; m < 60; m++)
 		{
-			for (l = 48; l <= 53; l++)
-			{
-				for (k = 48; k <= 57; k++)
-				{
-					_putchar(n);
-					_putchar(m);
-					if (n != 50 || m != 53)
-					{
-						_putchar(58);
-					}
-					_putchar(l);
-					_putchar(k);
-					if (l != 53 || k != 58)
-					{
-						_putchar('\n');
-					}
-				}
-			}
+			_putchar(n / 10 + 48);
+			_putchar(n % 10 + 48);
+			_putchar(58);
+			_putchar(m / 10 + 48);
+			_putchar(m % 10 + 48);
+			_putchar('\n');
 		}
 	}
 	return;
