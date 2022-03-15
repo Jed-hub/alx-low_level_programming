8b48cf79a37076d2271246c055396069330c5a9c
Jed <jedbahena@gmail.com>
2022-03-15 08:18:08 -0700

    Ok

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Prints _putchar
+ *
+ * Return: Always  0 (Success)
+ */
+int main(void)
+{
+	char s, p, u, t, c, h, a, r;
+
+	s = 95;
+	p = 112;
+	u = 117;
+	t = 116;
+	c = 99;
+	h = 104;
+	a = 97;
+	r = 114;
+	putchar(s);
+	putchar(p);
+	putchar(u);
+	putchar(t);
+	putchar(c);
+	putchar(h);
+	putchar(a);
+	putchar(r);
+	putchar('\n');
+	return (0);
+}
