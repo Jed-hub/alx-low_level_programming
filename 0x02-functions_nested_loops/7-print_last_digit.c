c7b50207363f9755b4f4a0efcb385c3e27efbc53
Jed <jedbahena@gmail.com>
2022-03-15 12:43:50 -0700

    Ok

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,6 +10,14 @@ int print_last_digit(int n)
 {
 	int result;
 
-	result = n % 100;
+	if (n < 0)
+	{
+		result = -1 * (n % 10);
+	}
+	else
+	{
+		result = n % 10;
+	}
+	_putchar(result + '0');	
 	return (result);
 }
