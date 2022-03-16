19c557743a3a30a43500443201d1963e7e3ec538
Jed <jedbahena@gmail.com>
2022-03-15 19:02:42 -0700

    Ok

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,7 +11,7 @@ int numLength(int num)
 
 	if (!num)
 	{
-		return(1);
+		return (1);
 	}
 	while (num)
 	{
@@ -33,6 +33,11 @@ int main(void)
 	while (i <= 98)
 	{
 		if (f1o > 0)
+		{
+			printf("%lu", f1o);
+		}
+		initial0s = numLength(mx) - 1 - numLength(f1);
+		while (f1o > 0 && initial0s > 0)
 		{
 			printf("%i", 0);
 			initial0s--;
