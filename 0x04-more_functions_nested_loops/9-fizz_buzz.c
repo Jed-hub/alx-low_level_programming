19540c4d04c571de48b7b7cba4e4c11fa634a632
Jed <jedbahena@gmail.com>
2022-03-17 16:40:42 -0700

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,17 +14,17 @@ int main(void)
 
 	while (n <= 100)
 	{
-		if (((n % 3) == 0) && ((n % 5) == 0))
+		if (n % 3 == 0 && n % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if ((n % 3) == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if ((n % 5) == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
