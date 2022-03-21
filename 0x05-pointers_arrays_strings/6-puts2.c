5e2021d6bee6327b9621968fa5da99f772c5fb26
Jed <jedbahena@gmail.com>
2022-03-21 16:55:05 -0700

    Ok

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints char
+ * @str: Array of string to reverse
+ */
+void puts2(char *str)
+{
+	int i, j;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; j < i; j += 2)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}
