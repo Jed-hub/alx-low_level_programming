5b58df39d10bd629ae6740cf4c66a5004a8febc5
Jed <jedbahena@gmail.com>
2022-04-13 11:39:54 -0700

    First commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Prints a name.
+ * @name: The name to be printed.
+ * @f: The pointer function.
+ *
+ * Return: Nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
