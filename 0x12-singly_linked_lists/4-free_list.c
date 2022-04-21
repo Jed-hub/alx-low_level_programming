69981cce0074f242a348f5cae774369b855380c9
Jed <jedbahena@gmail.com>
2022-04-21 16:48:02 -0700

    Write new code

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: the head of the node
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current_node;
+	list_t *next_node;
+
+	current_node = head;
+
+	while (current_node != NULL)
+	{
+		next_node = current_node->next;
+		free(current_node->str);
+		free(current_node);
+		current_node = next_node;
+	}
+}
