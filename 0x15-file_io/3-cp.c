72d68713ddd9987df6c119e8b2c3528c773b3f00
Jed <jedbahena@gmail.com>
2022-05-02 10:53:26 -0700

    Fix bug

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,7 +18,7 @@ int main(int ac, char **av)
 	file_from = open(av[1], O_RDONLY);
 	if (file_from == -1)
 	{dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]),
-	exit(97); }
+	exit(98); }
 	file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file_to == -1)
 	{dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99); }
